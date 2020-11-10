01398eff1dcef6b21d2f283baed5d50cf999888a
kilev <kooler.kilev@gmail.com>
2020-11-11 01:08:23 +0700

    add tutor-chat build

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,17 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('tutor-chat') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/kilev/tutorChat.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
