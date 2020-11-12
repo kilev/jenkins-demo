cbf5da96ce0b4816d1035d24b598795f7ffab332
kilev <kooler.kilev@gmail.com>
2020-11-12 22:19:25 +0700

    tmp

--- createJobs.groovy
+++ createJobs.groovy
@@ -12,33 +12,3 @@ pipelineJob('tutor-chat') {
         }
     }
 }
-pipelineJob('tutor-chat-docker') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url 'https://github.com/kilev/tutorChat.git'
-                    }
-                    branch 'master'
-                    scriptPath('Jenkinsfile-docker')
-                }
-            }
-        }
-    }
-}
-pipelineJob('tutor-chat-aws') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url 'https://github.com/kilev/tutorChat.git'
-                    }
-                    branch 'master'
-                    scriptPath('Jenkinsfile-aws')
-                }
-            }
-        }
-    }
-}
